c5e0a2983133bd4fd1ef4fffeca11447a55f41ca
CarlosGozalbes <carlosfgmendoza@gmail.com>
2023-03-16 02:51:49 +0100

    navigation

---
+++ scriptLinkNavbar.js
@@ -0,0 +1,16 @@
+// Get the current page's filename
+const currentPage = window.location.pathname.split("/").pop();
+console.log(currentPage)
+// Get all the navigation links
+const navLinks = document.querySelectorAll("nav a");
+console.log(navLinks)
+// Loop through each navigation link
+navLinks.forEach((link) => {
+  // Get the linked page's filename
+  const linkedPage = link.getAttribute("href").split("/").pop();
+
+  // If the current page matches the linked page, add the 'active' class to the link
+  if (currentPage === linkedPage) {
+    link.classList.add("active");
+  }
+});
